YildirimBasoglu/kotlin

commit 705a2881fab109ab7d3e365255d01e0491b17d6d

Freezable.{frozen, unfrozen} refactoring: add type safety

compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/Freezable.kt
@@ -40,8 +40,10 @@ abstract class Freezable {
 
     private var frozen: Boolean = false
 
-    private fun getInstanceWithFreezeStatus(value: Boolean) = if (value == frozen) this else copyBean(this).apply { frozen = value }
-
-    fun frozen() = getInstanceWithFreezeStatus(true)
-    fun unfrozen() = getInstanceWithFreezeStatus(false)
+    internal fun getInstanceWithFreezeStatus(value: Boolean) = if (value == frozen) this else copyBean(this).apply { frozen = value }
 }
+
+@Suppress("UNCHECKED_CAST")
+fun <T : Freezable> T.frozen(): T = getInstanceWithFreezeStatus(true) as T
+@Suppress("UNCHECKED_CAST")
+fun <T : Freezable> T.unfrozen(): T = getInstanceWithFreezeStatus(false) as T

idea/idea-jps-common/src/org/jetbrains/kotlin/config/KotlinFacetSettings.kt
@@ -7,10 +7,7 @@ package org.jetbrains.kotlin.config
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import org.jetbrains.kotlin.cli.common.arguments.Argument
-import org.jetbrains.kotlin.cli.common.arguments.CommonCompilerArguments
-import org.jetbrains.kotlin.cli.common.arguments.copyBean
-import org.jetbrains.kotlin.cli.common.arguments.parseCommandLineArguments
+import org.jetbrains.kotlin.cli.common.arguments.*
 import org.jetbrains.kotlin.platform.IdePlatformKind
 import org.jetbrains.kotlin.platform.TargetPlatform
 import org.jetbrains.kotlin.platform.TargetPlatformVersion
@@ -184,13 +181,13 @@ class KotlinFacetSettings {
 
     var compilerArguments: CommonCompilerArguments? = null
         set(value) {
-            field = value?.unfrozen() as CommonCompilerArguments?
+            field = value?.unfrozen()
             updateMergedArguments()
         }
 
     var compilerSettings: CompilerSettings? = null
         set(value) {
-            field = value?.unfrozen() as CompilerSettings?
+            field = value?.unfrozen()
             updateMergedArguments()
         }
 

commit 52edf0d6a20cb77932b77e7b5f8ea0369884673e

Freezable: fix binary compatibility
Intellij API Compatibility Check reports that compatibility was broken
for IJ Bazel plugin. This commit fixes this problem.

Binary compatibility was broken in
705a2881fab109ab7d3e365255d01e0491b17d6d.

Ideally, I'd rename extension functions to names other than "frozen" and
"unfrozen" because this would allow keeping source compatibility for
Java clients as well. But the new extension functions API has been
already released in EAP versions of IDEA, and I don't want change the
API back and forth for clients that already migrated to the new API. I
wish I knew earlier that this API is used by external plugins

compiler/cli/cli-common/src/org/jetbrains/kotlin/cli/common/arguments/Freezable.kt
@@ -41,9 +41,21 @@ abstract class Freezable {
     private var frozen: Boolean = false
 
     internal fun getInstanceWithFreezeStatus(value: Boolean) = if (value == frozen) this else copyBean(this).apply { frozen = value }
+
+    @Deprecated(level = DeprecationLevel.HIDDEN, message = "Please use type safe extension functions")
+    fun frozen() = getInstanceWithFreezeStatus(true)
+
+    @Deprecated(level = DeprecationLevel.HIDDEN, message = "Please use type safe extension functions")
+    fun unfrozen() = getInstanceWithFreezeStatus(false)
 }
 
-@Suppress("UNCHECKED_CAST")
+@Suppress(
+    "UNCHECKED_CAST",
+    "EXTENSION_SHADOWED_BY_MEMBER" // It's false positive shadowed warning KT-21598
+)
 fun <T : Freezable> T.frozen(): T = getInstanceWithFreezeStatus(true) as T
-@Suppress("UNCHECKED_CAST")
+@Suppress(
+    "UNCHECKED_CAST",
+    "EXTENSION_SHADOWED_BY_MEMBER" // It's false positive shadowed warning KT-21598
+)
 fun <T : Freezable> T.unfrozen(): T = getInstanceWithFreezeStatus(false) as T